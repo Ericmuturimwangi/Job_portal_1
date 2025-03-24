da388021568790768b378967df26ecb2a0a8ed44
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-03-24 21:39:43 +0300

    adding redis to application and employer dashboard

--- jobs/views.py
+++ jobs/views.py
@@ -17,22 +17,27 @@ def job_list(request):
 
 @login_required
 def apply_for_job(request, job_id):
-    job = get_object_or_404(Job, id = job_id)
-    form = JobApplicationForm()
-    if request.method == 'POST':
-        form = JobApplicationForm(request.POST, request.FILES)
-        if form.is_valid():
-            application =form.save(commit=False)
-            application.job = job
-            application.candidate = request.user
-            application.save ()
-            return redirect('job_list')
-        
-        else:
-            form = JobApplicationForm()
+        job = cache.get(f'job_{job_id}')
 
-    return render(request, 'jobs/apply_for_job.html', {'form':form, 'job': job})
-    
+        if not job:
+            job =get_object_or_404(Job, id=job_id)
+            cache.set(f'job_{job_id}', job, timeout=500)
+        
+        form = JobApplicationForm()
+        if request.method == 'POST':
+            form = JobApplicationForm(request.POST, request.FILES)
+            if form.is_valid():
+                application =form.save(commit=False)
+                application.job = job
+                application.candidate = request.user
+                application.save ()
+                return redirect('job_list')
+            
+            else:
+                form = JobApplicationForm()
+
+        return render(request, 'jobs/apply_for_job.html', {'form':form, 'job': job})
+        
 @login_required
 def employer_dashboard(request):
     if not hasattr(request.user, 'profile'):
@@ -42,7 +47,11 @@ def employer_dashboard(request):
         return redirect ('job_list')
     
 
-    applications  = JobApplication.objects.filter(job__user = request.user)
+    applications = cache.get(f'employer_{request.user.id}_applications')
+
+    if not applications:
+        applications = JobApplication.objects.filter(job__user=request.user)
+        cache.set(f'employer_{request.user.id}_applications', applications, timeout=900)
     
     return render (request, 'jobs/employer_dashboard.html', {'applications':applications})
 
