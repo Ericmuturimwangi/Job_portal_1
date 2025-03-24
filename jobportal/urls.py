98c0c2210bb31bb4741e022c3d3013eac71a6599
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-03-24 21:12:05 +0300

    all_auth methods

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path ("", include('jobs.urls')),
     path('accounts/', include ('allauth.urls')),
+    path('accounts/logout/', include('allauth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
